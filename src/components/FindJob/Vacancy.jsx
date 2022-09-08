665f54590c9ca238cd34ca95bc1ad745f5daff00
ykrykun <yura.krykun@gmail.com>
2022-09-08 11:44:15 +0300

    add styles 2

--- src/components/FindJob/Vacancy.jsx
+++ src/components/FindJob/Vacancy.jsx
@@ -3,12 +3,12 @@ import classes from './Vacancy.module.css';
 
 const Vacancy = () => {
     return(
-        <div className = {classes.vacancyBlock}>
-            <div className = {classes.block}>
-                <div className = {classes.satisfaction}> <img className = {classes.satisfactionImg} src="img/satisfaction.svg" alt="" /> 4 отзыва</div>
+        <div className = {classes.vacancy_card}>
+            <div className = {classes.card_body}>
+                <div className = {classes.rating}> <img className = {classes.rating_img} src="img/satisfaction.svg" alt="" /> 4 отзыва</div>
                 <div className = {classes.save}>Сохранить</div>
             </div>
-            <div className = {classes.block}>
+            <div className = {classes.card_body}>
                 <div className ={classes.vacancyTitle}>
                     <h3>Завод Bosh (Сбор бытовой техники) от 3680 zl <br /> Семейные пары</h3>
                     <p>Агенство “Тамара”</p>    
@@ -19,9 +19,9 @@ const Vacancy = () => {
                 <p>Официальное трудоустройство (umowa zlecenia).
 Комплект постельного белья предоставляется по желанию (стоимость 50 злотых) высчитывается с зарплаты. Рабочая одежда выдается бесплатно.</p>
             </div>
-            <div className = {classes.block}>
+            <div className = {classes.card_body}>
                 <div>Польша, Лодзь</div>
-                <div className = {classes.block}> 
+                <div className = {classes.card_body}> 
                     <div className = {classes.footerAny}>безопасность</div>
                     <div className = {classes.footerAny}>срочность</div>
                 </div>
